d9d4e74018e8643eb624417be1501c72af4e288e
b1tzxrd <timkatey2004@gmail.com>
2025-10-27 00:11:14 +0500

    feat: added a solution to the task from: streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import { createReadStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-  // Write your code here
+  const filePath = join(__dirname, "files", "fileToRead.txt");
+
+  const stream = createReadStream(filePath);
+
+  stream.on("data", (chunk) => process.stdout.write(chunk));
+
+  stream.on("end", () => {
+    process.stdout.write("\n");
+  });
+
+  stream.on("error", (err) => {
+    console.error(`error reading file: ${err.message}`)
+  })
 };
 
 await read();
