190dfcf8bd1f4a5573794cfe472c5cc304d3c1b8
b1tzxrd <timkatey2004@gmail.com>
2025-10-26 22:26:27 +0500

    init: start fs/delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,12 @@
+import fs from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const remove = async () => {
-  // Write your code here
+  
 };
 
 await remove();
