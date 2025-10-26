9bcd824a41de226a4067b8b19f22767ac5624db5
b1tzxrd <timkatey2004@gmail.com>
2025-10-26 22:03:27 +0500

    feat: added a solution to the task from copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import fs from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const copy = async () => {
-  // Write your code here
+  const srcDir = join(__dirname, "files");
+  const copyDir = join(__dirname, "files_copy");
+
+  try {
+    await fs.access(srcDir);
+
+    try {
+      await fs.access(copyDir);
+      console.error("FS operation failed");
+    } catch {
+      await fs.cp(srcDir, copyDir, { recursive: true })
+    }
+  } catch {
+    console.error("FS operation failed");
+  }
 };
 
 await copy();
