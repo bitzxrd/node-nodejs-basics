704e67aa1a920ff0a736ed8bb8e6ac513ca482e4
b1tzxrd <timkatey2004@gmail.com>
2025-10-26 21:53:07 +0500

    feat: added a solution to the task from create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = dirname(__filename);
+
 const create = async () => {
-  // Write your code here
+  const filePath = join(__dirname, "files", "fresh.tsxh")
+
+  try {
+    await fs.access(filePath);
+    console.error("FS operation failed");
+  } catch {
+    try {
+      await fs.writeFile(filePath, "I am fresh and young", "utf8");
+    } catch {
+      console.error("FS operation failed");
+    }
+  }
 };
 
 await create();
