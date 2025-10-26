6ef75fb88180f833598f2e7225706fbb23be9be3
b1tzxrd <timkatey2004@gmail.com>
2025-10-26 23:16:23 +0500

    feat: added a solution to the task from env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-  // Write your code here
+  const processEnv = process.env;
+  let variablesRSS = '';
+
+  for (const key in processEnv) {
+    if (key.startsWith('RSS_')) {
+      variablesRSS += `${key}=${processEnv[key]}; `;
+    }
+  }
+
+  console.log(variablesRSS.trim());
 };
 
 parseEnv();
