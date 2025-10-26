8061c4c07b311d45d6fed3de9913b71760f7e79f
b1tzxrd <timkatey2004@gmail.com>
2025-10-27 00:17:41 +0500

    feat: added the solution to the task from streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,26 @@
-const write = async () => {
+import { createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const write = async () => {
+  const filePath = join(__dirname, "files", "fileToWrite.txt");
+
+  const writebleStream = createWriteStream(filePath, "utf-8");
+
+  console.log("write text");
+
+  process.stdin.pipe(writebleStream);
+
+  writebleStream.on("finish", () => {
+    console.log("Data successfully saved");
+  });
+
+  writebleStream.on("error", (err) => {
+    console.error("File write error: ", err);
+  })
 };
 
 await write();
