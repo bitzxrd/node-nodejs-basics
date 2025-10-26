fff01e3848c12fcbf171467f4e07a0b3ddd66e00
b1tzxrd <timkatey2004@gmail.com>
2025-10-27 00:55:19 +0500

    feat: added the solution to the task from wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,40 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { Worker } from "worker_threads";
+import os from "os";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-  // Write your code here
+  const numCores = os.cpus().length;
+  const numbersToCalculate = Array.from({ length: numCores }, (_, i) => 10 + i);
+
+  const results = [];
+
+  const workerPromises = numbersToCalculate.map((num, index) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(join(__dirname, "worker.js"));
+
+      worker.on("message", (msg) => {
+        results[index] = msg;
+        resolve(msg);
+        worker.terminate();
+      });
+
+      worker.on("error", () => {
+        results[index] = { status: "error", data: null };
+        resolve(results[index]);
+        worker.terminate();
+      });
+
+      worker.postMessage(num);
+    });
+  });
+
+  await Promise.all(workerPromises);
+
+  console.log(results);
 };
 
 await performCalculations();
