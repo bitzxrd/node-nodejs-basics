586b943216ccb548e384b3d0ebdfeea8f5dbf66e
b1tzxrd <timkatey2004@gmail.com>
2025-10-27 00:40:09 +0500

    feat: added the solution to the task from zip/decompress.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,31 @@
-const decompress = async () => {
+import { createReadStream, createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { createGunzip } from "zlib";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const decompress = async () => {
+  const inputFilePath = join(__dirname, "files", "archive.gz");
+  const outputFilePath = join(__dirname, "files", "fileToCompress.txt");
+
+  const readStream = createReadStream(inputFilePath);
+  const writeStream = createWriteStream(outputFilePath);
+  const gunzip = createGunzip();
+
+  readStream.pipe(gunzip).pipe(writeStream);
+
+  writeStream.on("finish", () => {
+    console.log("file successfully decompressed");
+  });
+
+  readStream.on("error", (err) =>
+    console.error(`error reading file ${err.message}`)
+  );
+  writeStream.on("error", (err) =>
+    console.error(`error write file ${err.message}`)
+  );
 };
 
 await decompress();
