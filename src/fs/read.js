ec7298da29c3fde0739779a9b384735984f7ca72
b1tzxrd <timkatey2004@gmail.com>
2025-10-26 22:45:22 +0500

    feat: added a solution to the task from read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
-const read = async () => {
+import fs from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const read = async () => {
+  const fileToRead = join(__dirname, "files", "fileToRead.txt");
+
+  try {
+    await fs.access(fileToRead);
+
+    try {
+      const readFile = await fs.readFile(fileToRead, 'utf-8');
+      console.log(readFile);
+    } catch {
+      console.error("FS operation failed");
+    }
+  } catch {
+    console.error("FS operation failed");
+  }
 };
 
 await read();
